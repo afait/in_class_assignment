02c4ffc913d423c16ceb6bdfacfaca6fe093ecaf
Annabelle Fait <imannaf8@gmail.com>
2025-01-30 16:15:28 -0500

    connect html to js file

--- in_class_assignment.js
+++ in_class_assignment.js
@@ -1,2 +1,20 @@
 // Task 1
+let x = 10;
+if (x > 5) {
+   console.log("Help") ;
+};
 
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+   console.log("Managable Expenses");
+};
+
+let score = 60
+if (score > 59) {
+    console.log("Passed")
+} else {
+    console.log("Failed")
+}
